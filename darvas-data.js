85b9aecae2ea2ac1719ca5cb6b85810d5f40d6a3
Abhishek Sen <abhisheksen77777@gmail.com>
2026-05-10 14:43:08 +0530

    Image Automate Update

---
+++ darvas-data.js
@@ -0,0 +1,30 @@
+window.DARVAS_ITEMS = [
+  {
+    title: "Nestle",
+    type: "RH&S",
+    setup: "Reverse Head & Shoulders",
+    image: "image/nestle.png",
+    chart: "https://www.tradingview.com/chart/?symbol=NSE:NESTLEIND"
+  },
+  {
+    title: "KWIL",
+    type: "VCP",
+    setup: "VCP",
+    image: "image/KWIL.png",
+    chart: "https://www.tradingview.com/chart/?symbol=NSE:KWIL"
+  },
+  {
+    title: "KRISHNADEF",
+    type: "Monthly Breakout",
+    setup: "Monthly Breakout",
+    image: "image/KRISHNADEF.png",
+    chart: "https://www.tradingview.com/chart/?symbol=NSE:KRISHNADEF"
+  },
+  {
+    title: "SCI",
+    type: "VCP",
+    setup: "VCP",
+    image: "image/SCI.png",
+    chart: "https://www.tradingview.com/chart/?symbol=NSE:SCI"
+  }
+];
